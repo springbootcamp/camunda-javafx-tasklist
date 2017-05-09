06ed598c8dffc490cb52b9c2b34cc56436e39a99
Jan Galinski <jan.galinski@holisticon.de>
2017-05-10 00:42:02 +0200

    initial

---
+++ src/test/java/org/springbootcamp/camunda/JavaFxTasklistApplicationTest.java
@@ -0,0 +1,16 @@
+package org.springbootcamp.camunda;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaFxTasklistApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
