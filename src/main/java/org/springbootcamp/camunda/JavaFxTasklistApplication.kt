e98a0156adbe985fd26abb8ea91e0c5865d5cbdd
Jan Galinski <jan.galinski@holisticon.de>
2017-05-10 00:41:52 +0200

    initial

---
+++ src/main/java/org/springbootcamp/camunda/JavaFxTasklistApplication.kt
@@ -0,0 +1,11 @@
+package org.springbootcamp.camunda
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class JavaFxTasklistApplication
+
+fun main(args: Array<String>) {
+  SpringApplication.run(JavaFxTasklistApplication::class.java, *args)
+}
